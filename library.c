c4eee6bb0c5d89fda6208b8f09369e53fa4ae6f3
floxo115 <flojak@gmx.net>
2024-12-14 19:26:04 +0100

    refactored resizing

--- library.c
+++ library.c
@@ -20,6 +20,18 @@ DArray *darray_init(size_t type_size, size_t cap) {
     return da;
 }
 
+bool resize_darray_(DArray *da) {
+    size_t new_cap = da->cap * 2;
+    void *new_data = realloc(da->data, da->type_size * new_cap);
+    if (new_data == NULL)
+        return false;
+
+    da->data = new_data;
+    da->cap = new_cap;
+
+    return true;
+}
+
 void *darray_access(DArray *da, size_t pos) {
     if (da == NULL)
         return NULL;
@@ -36,15 +48,8 @@ bool darray_append(DArray *da, void *value) {
     if (value == NULL)
         return false;
 
-    if (da->len >= da->cap) {
-        size_t new_cap = da->cap * 2;
-        void *new_data = realloc(da->data, da->type_size * new_cap);
-        if (new_data == NULL)
-            return false;
-
-        da->data = new_data;
-        da->cap = new_cap;
-    }
+    if (da->len >= da->cap && !resize_darray_(da))
+        return false;
 
     memcpy((char *) da->data + da->type_size * da->len, value, da->type_size);
 
@@ -52,3 +57,29 @@ bool darray_append(DArray *da, void *value) {
 
     return true;
 }
+
+bool darray_insert(DArray *da, size_t pos, void *value) {
+    if (da == NULL || value == NULL)
+        return false;
+    if (pos > da->len)
+        return false;
+
+    if (pos == da->len)
+        return darray_append(da, value);
+
+    if (da->len >= da->cap && !resize_darray_(da))
+        return false;
+
+    memmove((char *) da->data + (pos + 1) * da->type_size,
+            (char *) da->data + da->type_size * pos,
+            (da->len - pos) * da->type_size
+    );
+    memcpy((char *) da->data + da->type_size * pos,
+           value,
+           da->type_size
+    );
+    da->len++;
+
+    return true;
+}
+
